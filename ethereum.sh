4186a13e378bec1df417340727f3c678d759722f
yono1996 <39361116+yono1996@users.noreply.github.com>
2024-04-09 14:45:34 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-532f7268-2241-41df-b00d-40d4dd5b06d1/"
 
 OUTPUT_DIR="logs"
 
